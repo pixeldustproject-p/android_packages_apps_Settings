663f747f09b0619749cc0e6e418be73f3bd01082
Anushek Prasal <anushekprasal@gmail.com>
2018-11-23 17:00:58 +0530

    Make Font Picker play nice with substratum
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/display/FontPickerPreferenceController.java
+++ src/com/android/settings/display/FontPickerPreferenceController.java
@@ -20,36 +20,21 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.FontInfo;
 import android.content.IFontService;
-import android.content.pm.PackageManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceClickListener;
-import android.text.TextUtils;
-import android.util.Log;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
-import com.android.settings.display.AccentPicker;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
 
 public class FontPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String TAG = "FontPickerPreferenceController";
     private static final String KEY_FONT_PICKER_FRAGMENT_PREF = "custom_font";
-    private static final String SUBS_PACKAGE = "projekt.substratum";
     private FontDialogPreference mFontPreference;
     private IFontService mFontService;
 
@@ -67,22 +52,13 @@ public class FontPickerPreferenceController extends AbstractPreferenceController
         if (mFontPreference == null) {
             return;
         }
-        if (!isPackageInstalled(SUBS_PACKAGE, mContext)) {
-            mFontPreference.setSummary(getCurrentFontInfo().fontName.replace("_", " "));
-        } else {
-            mFontPreference.setSummary(mContext.getString(
-                    com.android.settings.R.string.disable_fonts_installed_title));
-        }
+        mFontPreference.setSummary(getCurrentFontInfo().fontName.replace("_", " "));
     }
 
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mFontPreference = (FontDialogPreference) screen.findPreference(KEY_FONT_PICKER_FRAGMENT_PREF);
-        if (!isPackageInstalled(SUBS_PACKAGE, mContext)) {
-            mFontPreference.setEnabled(true);
-        } else {
-            mFontPreference.setEnabled(false);
-        }
+        mFontPreference.setEnabled(true);
     }
 
     @Override
@@ -105,15 +81,5 @@ public class FontPickerPreferenceController extends AbstractPreferenceController
 
     public void stopProgress() {
         mFontPreference.stopProgress();
-    }
-
-    private boolean isPackageInstalled(String package_name, Context context) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            pm.getPackageInfo(package_name, PackageManager.GET_ACTIVITIES);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 }
