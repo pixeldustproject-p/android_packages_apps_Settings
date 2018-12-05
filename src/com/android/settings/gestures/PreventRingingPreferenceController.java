e2a66ada02efdd376b9d668ab747036e84c94505
spezi77 <spezi7713@gmx.net>
2018-12-05 21:10:10 +0100

    Revert "Settings: : option to mute media with gesture [2/2]"
    This reverts commit 99e702476818a63542ee716f417b8fbf97c53a74.

--- src/com/android/settings/gestures/PreventRingingPreferenceController.java
+++ src/com/android/settings/gestures/PreventRingingPreferenceController.java
@@ -18,7 +18,6 @@ package com.android.settings.gestures;
 
 import static android.provider.Settings.Secure.VOLUME_HUSH_GESTURE;
 import static android.provider.Settings.Secure.VOLUME_HUSH_MUTE;
-import static android.provider.Settings.Secure.VOLUME_HUSH_MUTE_NO_MEDIA;
 import static android.provider.Settings.Secure.VOLUME_HUSH_OFF;
 import static android.provider.Settings.Secure.VOLUME_HUSH_VIBRATE;
 
@@ -88,9 +87,6 @@ public class PreventRingingPreferenceController extends BasePreferenceController
                     case VOLUME_HUSH_MUTE:
                         pref.setValue(String.valueOf(value));
                         break;
-                    case VOLUME_HUSH_MUTE_NO_MEDIA:
-                        pref.setValue(String.valueOf(value));
-                        break;
                     default:
                         pref.setValue(String.valueOf(VOLUME_HUSH_OFF));
                 }
@@ -109,9 +105,6 @@ public class PreventRingingPreferenceController extends BasePreferenceController
                 break;
             case VOLUME_HUSH_MUTE:
                 summary = R.string.prevent_ringing_option_mute_summary;
-                break;
-            case VOLUME_HUSH_MUTE_NO_MEDIA:
-                summary = R.string.prevent_ringing_option_mute_no_media_summary;
                 break;
             default:
                 summary = R.string.prevent_ringing_option_none_summary;
